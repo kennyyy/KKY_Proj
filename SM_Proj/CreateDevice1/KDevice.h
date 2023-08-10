89855f5de15a0d419b99690de60a4b55c502644e
김경연 컴퓨터공학부 <kennyyy@kyonggi.ac.kr>
2023-08-10 22:01:22 +0900

    ssd

--- SM_Proj/CreateDevice1/KDevice.h
+++ SM_Proj/CreateDevice1/KDevice.h
@@ -5,29 +5,23 @@
 class KDevice : public KWindow
 {
 
+
 public:
-	IDXGISwapChain* m_pSwapChain = nullptr;
+	IDXGIFactory* m_pGIFacotry = nullptr;
 	ID3D11Device* m_pDevice = nullptr;
+	IDXGISwapChain* m_pSwapChain = nullptr;
 	ID3D11DeviceContext* m_pImmediateContext = nullptr;
 	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;
+	D3D_FEATURE_LEVEL       m_FeatureLevel;
+	D3D11_VIEWPORT			m_ViewPort;
 
-	D3D11_VIEWPORT			m_ViewPort;			// ºäÆ÷Æ®
-//public:
-//	IDXGIFactory* m_pGIFacotry = nullptr;
-//	ID3D11Device* m_pDevice = nullptr;
-//	IDXGISwapChain* m_pSwapChain = nullptr;
-//	ID3D11DeviceContext* m_pImmediateContext = nullptr;
-//	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;
-//	D3D_FEATURE_LEVEL       m_FeatureLevel;
-//	D3D11_VIEWPORT			m_ViewPort;
-//
-//public:
-//	HRESULT CreateDevice();
-//	HRESULT CreateGIFactory();
-//	HRESULT CreateSwapChain();
-//
-//	HRESULT SetRenderTargetView();
-//	HRESULT SetViewPort();
+public:
+	HRESULT CreateDevice();
+	HRESULT CreateGIFactory();
+	HRESULT CreateSwapChain();
+
+	HRESULT SetRenderTargetView();
+	HRESULT SetViewPort();
 
 
 
